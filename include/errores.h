3172a46274ab111a3b62a73fe2d1a94b20ca1ea6
Fernando Giráldez Curquejo <122482852+Raquezin@users.noreply.github.com>
2025-11-13 11:46:15 +0100

    PLU solve (#2)
    * PLU decomp method
    
    * writing solve_PLU
    
    * writing solve_PLU and security inverse

--- include/errores.h
+++ include/errores.h
@@ -5,6 +5,8 @@ typedef enum {
     ERR_DIM_MISMATCH = -1,
     ERR_NONE,
     ERR_DET_0,
+    ERR_INF_SOL,
+    ERR_NO_SOL
 } ErrorCode;
 
 #endif // ERRORES_H
--- src/operations.c
+++ src/operations.c
@@ -69,7 +69,7 @@ ErrorCode inverse(Matrix a, Matrix r){
     float d;
     determinant(a, r, &d);
     
-    if (d == 0)
+    if (fabs(d) < 1e-9)
         return ERR_DET_0;
 
     fill(r, 0);
@@ -132,6 +132,10 @@ ErrorCode inverse(Matrix a, Matrix r){
 }
 
 ErrorCode decomp_PLU(const Matrix a, Matrix L, Matrix U, Matrix P) {
+    /*
+    You have PA = LU
+    Ax = b
+    */
     if (a.row != a.col || L.row != a.row || L.col != a.col || U.row != a.row 
         || U.col != a.col || P.row != a.row || P.col != a.col)
         return ERR_DIM_MISMATCH;
@@ -176,5 +180,44 @@ ErrorCode decomp_PLU(const Matrix a, Matrix L, Matrix U, Matrix P) {
     }
 
     identity(L);
+    return ERR_NONE;
+}
+
+ErrorCode solve_PLU(const Matrix a, const Matrix L, const Matrix U,
+                    const Matrix P, const Matrix b, Matrix x) {
+    if (a.row != a.col || L.row != a.row || L.col != a.col 
+        || U.row != a.row || U.col != a.col 
+        || P.row != a.row || P.col != a.col
+        || b.row != a.row || b.col != 1 
+        || a.row != x.row || x.col != 1)
+        return ERR_DIM_MISMATCH;
+
+    float y[a.row];
+    multiply(P, b, x);
+
+    int base;
+    for (int i = 0; i < a.row; i++) { // Ly = Pb
+        y[i] = x.data[i];
+        base = i * a.col;
+        for (int j = 0; j < i; j++) {
+            y[i] -= L.data[base + j]*y[j];
+        }
+    } 
+
+    for (int i = a.row - 1; i >= 0; i--) { // Ux = y
+        x.data[i] = y[i];
+        base = i * a.col;
+        for (int j = a.col - 1; j >= i + 1; j--) {
+            x.data[i] -= U.data[base + j]*x.data[j];
+        }
+        if (fabs(U.data[base + i]) < 1e-9) { 
+            if (fabs(x.data[i]) < 1e-9) { // Caso 0 = 0
+                    return ERR_INF_SOL;
+            } else { // Caso 0 = (no cero)
+                return ERR_NO_SOL;
+            }
+        }
+        x.data[i] /= U.data[base + i];
+    } 
     return ERR_NONE;
 }
